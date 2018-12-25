bd9028f003f84152fcdaae125fd774283c2527c2
loc nguyen <43066715+locnguyen1402@users.noreply.github.com>
2018-12-26 00:14:20 +0700

    Update db.js
    hoàn thành

--- models/db.js
+++ models/db.js
@@ -13,24 +13,28 @@ let mcdonald_db = mongoose.model('mcdonald_db', StoreSchema);
 let timeArray = ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00'];
 //tìm cửa hàng người dùng chọn
 async function getValidStore(store_id, day) {
+    let store;
     if (day == 'today') {
-        var store = await mcdonald_db.find({
+        store = await mcdonald_db.find({
             id: store_id
         }, {
+            id: 1,
             today: 1
         });
     }
     if (day == 'tomorrow') {
-        var store = await mcdonald_db.find({
+        store = await mcdonald_db.find({
             id: store_id
         }, {
+            id: 1,
             tomorrow: 1
         });
     }
     if (day == 'thatday') {
-        var store = await mcdonald_db.find({
+        store = await mcdonald_db.find({
             id: store_id
         }, {
+            id: 1,
             thatday: 1
         });
     }
@@ -105,55 +109,71 @@ function findFeasibleNumberOfTables(indexTime, store) {
     return tableArray;
 }
 
-//đặt bàn
-function booking(indexTime, statusArray) {
+//trả về object để đặt bàn, chuyền vào mảng các bàn có thể đặt, số bàn cần đặt, ngày đặt (dayKey)
+function objectUpdate(tempObj, tableArray, dayKey, statusArray, indexTime) {
     for (let i = 0; i < 4; i++) {
-        statusArray[index + i] = true;
+        statusArray[indexTime + i] = true;
     }
 
-    mcdonald.findOneAndUpdate({
-        id: 'store00'
+    tempObj[`${dayKey}.${tableArray}.status`] = statusArray;
+}
+
+//đặt bàn
+async function booking(store_id, tempObj) {
+    await mcdonald_db.findOneAndUpdate({
+        id: store_id
     }, {
-        $set: {
-            "today.tb1.status": statusArray
-        }
-    }, (err, doc) => {
-        if (err) throw err;
-        console.log('updated');
+        $set: tempObj
     });
 }
 
-//truyền vào id store, ngày, giờ, số người, username 
-function bookingProcess(store_id, day, time, capacity, username) {
+//truyền vào id store, ngày, giờ, số người, username => chỉ cần gọi 1 hàm này
+function bookingProcess(store_id, day, time, capacity, res) {
     let amoutOfTables = findAmountOfTables(capacity);
     let indexTime = timeArray.indexOf(time);
     let dayKey = checkDate(day);
 
     if (dayKey == 'nomatch') {
         console.log('ngay khong phu hop');
+        //res.json('something');
     } else {
         //ngày phù hợp để đặt, kiểm tra tiếp
-        getValidStore(store_id, day).then((store) => {
+        getValidStore(store_id, dayKey).then((store) => {
             if (store.length == 0) {
                 //không có cửa hàng nên ko đặt được
                 console.log('không có cửa hàng nên ko đặt được');
+                //res.json('something');
             } else {
                 //kiểm tra có đủ bàn đặt ko
-                if (findFeasibleNumberOfTables(indexTime, store[0][`${dayKey}`]) < amoutOfTables) {
+                let tableArray = findFeasibleNumberOfTables(indexTime, store[0][`${dayKey}`]); // mảng bàn có thể đặt
+                if (tableArray.length < amoutOfTables) {
                     //không đủ bàn
                     console.log('không đủ bàn');
+                    //res.json('something');
                 } else {
-                    //đủ bàn
-                    //đặt bàn
+                    //đủ bàn => đặt bàn
+                    let tempObj = {};
+
+                    for (let i = 0; i < amoutOfTables; i++) {
+                        objectUpdate(tempObj, tableArray[i], dayKey, store[0][`${dayKey}`][`${tableArray[i]}`].status, indexTime);
+                    }
+                    booking(store[0].id, tempObj).then(() => {
+                        console.log('updated');
+                        //res.json('something');
+                    });
                 }
             }
         });
     }
 }
 
+bookingProcess('store00', '12/26/2018', '00:00', 3, 'res');
+
+
+
 /* getValidStore('store05', 'today').then((rel) => {
     console.log(rel);
     console.log('-----------------------');
 });  */
 
-module.exports = mcdonald_db;
+//module.exports = mcdonald_db;
